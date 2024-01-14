652696e59605b4ac3de20c4b1f8830eca299255d
MAKSIM <max_air@bk.ru>
2024-01-14 21:30:07 +0300

    Filter

--- src/components/heroesFilters/HeroesFilters.js
+++ src/components/heroesFilters/HeroesFilters.js
@@ -5,18 +5,44 @@
 // Активный фильтр имеет класс active
 // Изменять json-файл для удобства МОЖНО!
 // Представьте, что вы попросили бэкенд-разработчика об этом
+import { useSelector } from "react-redux";
+
 
 const HeroesFilters = () => {
+    const { filters } = useSelector(state => state);
+
+
+    
+
+    const renderFilters = (filters) => {
+        return filters.map(filter => {
+            switch (filter) {
+                case "fire":
+                    return <button className="btn btn-danger">Огонь</button>
+                case "water":
+                    return <button className="btn btn-primary">Вода</button>
+                case "wind":
+                    return <button className="btn btn-success">Ветер</button>
+                case "earth":
+                    return <button className="btn btn-secondary">Земля</button>
+                default:
+                    return <button className="btn btn-outline-dark active">Все</button>
+            }
+
+        })
+    }
+
     return (
         <div className="card shadow-lg mt-4">
             <div className="card-body">
                 <p className="card-text">Отфильтруйте героев по элементам</p>
                 <div className="btn-group">
-                    <button className="btn btn-outline-dark active">Все</button>
+                    {renderFilters(filters)}
+                    {/* <button className="btn btn-outline-dark active">Все</button>
                     <button className="btn btn-danger">Огонь</button>
                     <button className="btn btn-primary">Вода</button>
                     <button className="btn btn-success">Ветер</button>
-                    <button className="btn btn-secondary">Земля</button>
+                    <button className="btn btn-secondary">Земля</button> */}
                 </div>
             </div>
         </div>
